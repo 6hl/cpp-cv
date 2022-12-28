d2061d44ed07840040b6de2ac8a007bb4e81f523
Ben Liggett <bliggett@mitre.org>
2022-12-28 16:53:48 +0000

    Switched to test and run make capabilities

--- cfg/test.sh
+++ cfg/test.sh
@@ -3,4 +3,9 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/../build
 make --no-print-directory
-./detection
+
+full_path=$SCRIPT_DIR/../test/runs/$(date "+%F-%T")
+mkdir -p $full_path
+pass_path=$(cd $full_path; pwd)
+echo Test Saved at: $pass_path
+./tests $pass_path
