594e7358e18eb5e66a5e1d7fac0bced37631c53c
Ben Liggett <bliggett@mitre.org>
2023-01-03 19:39:41 +0000

    Improved readability

--- src/detector.cpp
+++ src/detector.cpp
@@ -13,17 +13,17 @@ cv::Mat format_img(const cv::Mat& img)
     return res;
 }
 
-cv::Rect to_yolo(float* data, float x_f, float y_f)
+cv::Rect to_yolo(float* data, float x_scale, float y_scale)
 {
     float x = data[0];
     float y = data[1];
     float w = data[2];
     float h = data[3];
-    int left = int((x - 0.5 * w) * x_f);
-    int top = int((y - 0.5 * h) * y_f);
-    int width = int(w * x_f);
-    int height = int(h * y_f);
-    return cv::Rect(left, top, width, height);
+    int scaled_x = int((x - 0.5 * w) * x_scale);
+    int scaled_y = int((y - 0.5 * h) * y_scale);
+    int scaled_w = int(w * x_scale);
+    int scaled_h = int(h * y_scale);
+    return cv::Rect(scaled_x, scaled_y, scaled_w, scaled_h);
 }
 
 std::vector<Object> nms(std::vector<cv::Rect> bboxs, std::vector<float> confidences, std::vector<int> class_ids, float SCORE_THRESH, float NMS_THRESH)
@@ -31,9 +31,8 @@ std::vector<Object> nms(std::vector<cv::Rect> bboxs, std::vector<float> confiden
     std::vector<Object> detections;
     std::vector<int> nms_result;
     cv::dnn::NMSBoxes(bboxs, confidences, SCORE_THRESH, NMS_THRESH, nms_result);
-    for (int i = 0; i < nms_result.size(); ++i)
+    for (auto& idx : nms_result)
     {
-        int idx = nms_result[i];
         Object object;
         object.class_id = class_ids[idx];
         object.confidence = confidences[idx];
@@ -50,8 +49,8 @@ std::vector<Object> detect(cv::Mat& img, Model* model, const std::vector<std::st
     std::vector<cv::Mat> model_output;
     model->net.forward(model_output, model->net.getUnconnectedOutLayersNames());
 
-    float x_f = img.cols / 640.;
-    float y_f = img.rows / 640.;
+    float x_scale = img.cols / 640.;
+    float y_scale = img.rows / 640.;
     float* data = reinterpret_cast<float*>(model_output[0].data);
     const int rows = model_output[0].size[2];                           // 85 for yolov5
     const int cols = model_output[0].size[1];                           // 25200 for 640x640
@@ -74,7 +73,7 @@ std::vector<Object> detect(cv::Mat& img, Model* model, const std::vector<std::st
             {
                 confidences.push_back(confidence);
                 class_ids.push_back(class_id.x);
-                bboxs.push_back(to_yolo(data, x_f, y_f));
+                bboxs.push_back(to_yolo(data, x_scale, y_scale));
             }
         }
         data += rows;
